966202db4aa81fafaa934e2fe67e4ec04eb48b50
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2023-10-22 02:34:24 +0530

    updaye

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use('/', express.static(path.resolve(__dirname,'frontend','dist')));
 
 
 app.get("/api/jokes", (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://raja-babu-93mi.vercel.app/');
   res.send([
     {
       id: 1,
